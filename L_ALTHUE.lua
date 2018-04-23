f9f9ecd1cdfdc27688d60ef214c7f9d542b1e40d
rafale77 <anhmantran@yahoo.com>
2018-04-22 21:54:51 -0700

    Update L_ALTHUE.lua

--- L_ALTHUE.lua
+++ L_ALTHUE.lua
@@ -605,6 +605,28 @@ local function rgb_to_cie(red, green, blue)
 	return x1, y1
 end
 
+
+local function application:rgbToHex(rgb)
+	local hexadecimal = '0X'
+	for key, value in pairs(rgb) do
+		local hex = ''
+		while(value > 0)do
+			local index = math.fmod(value, 16) + 1
+			value = math.floor(value / 16)
+			hex = string.sub('0123456789ABCDEF', index, index) .. hex			
+		end
+
+		if(string.len(hex) == 0)then
+			hex = '00'
+		elseif(string.len(hex) == 1)then
+			hex = '0' .. hex
+		end
+
+		hexadecimal = hexadecimal .. hex
+	end
+	return hexadecimal
+end
+
 ------------------------------------------------
 -- UPNP Actions Sequence
 ------------------------------------------------
